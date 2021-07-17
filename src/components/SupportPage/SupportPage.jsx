3786d04d512e4f3c65c30f88dead718fc042888b
Matthew Bouc <boucgm@gmail.com>
2021-07-16 19:35:29 -0500

    support page: input, button, input state, button handler- dispatches and history.push

--- src/components/SupportPage/SupportPage.jsx
+++ src/components/SupportPage/SupportPage.jsx
@@ -1,6 +1,25 @@
+import {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 function SupportPage() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [supportState, setSupportState] = useState('');
+
+    const handleNext = () => {
+        dispatch({
+            type: 'GET_SUPPORT',
+            payload: supportState
+        })
+        history.push('/comments');
+    }
+
     return(
-        <div></div>
+        <form>
+            <input onChange={(event) => setSupportState(event.target.value)} type="text" />
+            <button onClick={handleNext} type="button">NEXT</button>
+        </form>
     )
 }
 
