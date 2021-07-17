277e04c51f6d8dd00533001212bd48735f2813f7
Matthew Bouc <boucgm@gmail.com>
2021-07-17 00:07:45 -0500

    fixed routes in feedback.router.js.  Tested with postman - GET and POST functional serverside to db

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool');
+const pool = require('../modules/pool.js');
 
 router.get('/', (req,res) => {
     console.log('GETting database');
     pool.query('SELECT * FROM feedback;')
-}).then(result => {
-    res.send(result.rows);
-}).catch(error => {
-    console.log('Error GETting from database', error);
-    res.sendStatus(500);
+        .then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('Error GETting from database', error);
+        res.sendStatus(500);
+    });
 });
 
 router.post('/', (req,res) => {
@@ -22,12 +23,13 @@ router.post('/', (req,res) => {
     const queryText = `INSERT INTO feedback ("feeling", "understanding", "support", "comments")
         VALUES ($1, $2, $3, $4);`
     pool.query(queryText, [feeling, understanding, support, comments])
-}).then(response => {
-    console.log('Success POSTing to db', response);
-    res.sendStatus(201);
-}).catch(error => {
-    console.log('Error POSTing to db', error);
-    res.sendStatus(500);
+    .then(response => {
+        console.log('Success POSTing to db', response);
+        res.sendStatus(201);
+    }).catch(error => {
+        console.log('Error POSTing to db', error);
+        res.sendStatus(500);
+    });
 });
 
 module.exports = router;
