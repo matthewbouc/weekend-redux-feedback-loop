499c35a2fbf88559315ddf8b3c967d11ad0a06b6
Matthew Bouc <boucgm@gmail.com>
2021-07-16 19:30:28 -0500

    understanding page: input, button, input value useState, button handler- dispatches to reducer and history.push

--- src/components/UnderstandingPage/UnderstandingPage.jsx
+++ src/components/UnderstandingPage/UnderstandingPage.jsx
@@ -1,6 +1,26 @@
+import {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
 function UnderstandingPage() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [understandingState, setUnderstandingState] = useState('');
+
+    const handleNext = () => {
+        dispatch({
+            type: 'GET_UNDERSTANDING',
+            payload: understandingState
+        })
+        history.push('/support');
+    }
+
     return(
-        <div></div>
+        <form>
+            <input onChange={(event) => setUnderstandingState(event.target.value)} type="text" />
+            <button onClick={handleNext} type="button">NEXT</button>
+        </form>
     )
 }
 
