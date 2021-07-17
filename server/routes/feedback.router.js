eeb26d81fb98f9035eb7416f1718e17e97c12d0d
Matthew Bouc <boucgm@gmail.com>
2021-07-16 23:56:36 -0500

    server routes to db created (GET and POST). created necessary changes in server.js and new folder/file

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.get('/', (req,res) => {
+    console.log('GETting database');
+    pool.query('SELECT * FROM feedback;')
+}).then(result => {
+    res.send(result.rows);
+}).catch(error => {
+    console.log('Error GETting from database', error);
+    res.sendStatus(500);
+});
+
+router.post('/', (req,res) => {
+    const {
+        feeling,
+        understanding,
+        support,
+        comments
+    } = req.body;
+    const queryText = `INSERT INTO feedback ("feeling", "understanding", "support", "comments")
+        VALUES ($1, $2, $3, $4);`
+    pool.query(queryText, [feeling, understanding, support, comments])
+}).then(response => {
+    console.log('Success POSTing to db', response);
+    res.sendStatus(201);
+}).catch(error => {
+    console.log('Error POSTing to db', error);
+    res.sendStatus(500);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+const feedbackRouter = require('./routes/feedback.router.js');
+app.use('/api/feedback', feedbackRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
