9d902ce930c199f7b449ea0dc63f8415a305ba1d
Matthew Bouc <boucgm@gmail.com>
2021-07-16 19:11:38 -0500

    reducers in place

--- src/index.js
+++ src/index.js
@@ -3,6 +3,54 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger'
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const feelingReducer = (state = {}, action) => {
+    if (action.type === 'GET_FEELINGS'){
+        return action.payload
+    }
+    return state;
+}
+
+const understandingReducer = (state = {}, action) => {
+    if (action.type === 'GET_UNDERSTANDING'){
+        return action.payload
+    }
+    return state;
+}
+
+const supportReducer = (state = {}, action) => {
+    if (action.type === 'GET_SUPPORT'){
+        return action.payload
+    }
+    return state;
+}
+
+const commentReducer = (state = {}, action) => {
+    if (action.type === 'GET_COMMENT'){
+        return action.payload
+    }
+    return state;
+}
+
+
+const storeInstance = createStore(
+    combineReducers({
+        feelingReducer,
+        understandingReducer,
+        supportReducer,
+        commentReducer    
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
